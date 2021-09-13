75e67c1de36dcf78997bcb29d085d6ef5b78b0ee
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-13 23:55:27 +0900

    Router Setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import firebase from "../firebase";
+import React from "react";
+import AppRouter from "./Router";
+
+console.log(firebase);
+
+function App() {
+  return <AppRouter />;
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+  return (
+    <Router exact path="/">
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route>
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+export default AppRouter;

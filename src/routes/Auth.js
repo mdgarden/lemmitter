e183f7ee331a11aef8e6f5ee85fcbf4e6b947689
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-18 02:13:43 +0900

    Log In

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -9,6 +9,7 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
   const onChange = (event) => {
     const {
@@ -36,6 +37,7 @@ const Auth = () => {
           // ..
           console.log(errorCode);
           console.log(errorMessage);
+          setError(error.message);
         });
     } else {
       signInWithEmailAndPassword(authService, email, password)
@@ -50,9 +52,12 @@ const Auth = () => {
           const errorMessage = error.message;
           console.log(errorCode);
           console.log(errorMessage);
+          setError(error.message);
         });
     }
   };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
 
   return (
     <div>
@@ -73,8 +78,15 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>

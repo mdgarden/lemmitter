42631d94f89a1cd9b21cb9a78784f4cba3fcebc5
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-21 22:10:43 +0900

    database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
 
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="무슨 일이 일어나고 있나요?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;

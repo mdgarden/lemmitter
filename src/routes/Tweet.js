7e1fc9e28e00382bc52ee078d769745974a298f7
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-26 05:53:49 +0900

    Delete and Update part Two

--- src/routes/Tweet.js
+++ src/routes/Tweet.js
@@ -1,13 +1,56 @@
+import { deleteDoc, updateDoc, doc } from "@firebase/firestore";
+import { dbService } from "fbase";
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
 const Tweet = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this tweet?");
+    if (ok) {
+      await deleteDoc(doc(dbService, "tweets", tweetObj.id));
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(doc(dbService, "tweets", tweetObj.id), {
+      text: newTweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewTweet(value);
+  };
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Tweet</button>
-          <button>Edit Tweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your tweet"
+              value={newTweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Tweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Tweet</button>
+              <button onClick={toggleEditing}>Edit Tweet</button>
+            </>
+          )}
         </>
       )}
     </div>

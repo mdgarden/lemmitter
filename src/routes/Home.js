e1df2205d351ac332e64d7537625a05d12f8e3df
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-09-28 03:31:58 +0900

    Preview Images part Two

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,6 +12,7 @@ import Tweet from "./Tweet";
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
     const q = query(
@@ -54,9 +55,15 @@ const Home = ({ userObj }) => {
     } = event;
     const theFile = files[0];
     const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {};
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment();
+    };
     reader.readAsDataURL(theFile);
   };
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <div>
@@ -70,6 +77,12 @@ const Home = ({ userObj }) => {
         />
         <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="tweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="" width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       {tweets.map((tweet) => (
         <Tweet

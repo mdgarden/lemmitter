1dd44c41499d29e6359978c3b2587beeb338fe76
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-10-02 20:51:32 +0900

    Get My Own Tweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
+import { collection, getDocs, query, where } from "@firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyTweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
